fa0eafdede3b51ce684a922b2cf83bddd0045a9e
rinse <rinse@neko2.net>
2020-08-24 22:28:53 +0900

    separate V1 into a package

--- src/Server.hs
+++ src/Server.hs
@@ -1,98 +1,35 @@
-{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Server where
 
-import           Control.Concurrent.STM    (TVar, atomically, newTVar)
-import           Control.Exception.Safe    (MonadCatch, MonadThrow, catchAny,
-                                            throw)
-import           Control.Monad
+import           Control.Concurrent.STM     (TVar, atomically, newTVar)
 import           Control.Monad.Cont
-import           Control.Monad.State.Class (get, put)
-import           Data.Aeson.Text           (encodeToLazyText)
-import qualified Data.ByteString.Lazy      as BL
-import           Data.Text                 (Text)
-import qualified Data.Text                 as T
-import qualified Data.Text.IO              as T
-import qualified Data.Text.Lazy            as TL
-import qualified Network.Wai.Handler.Warp  as Warp
+import qualified Data.ByteString.Lazy       as BL
+import qualified Network.Wai.Handler.Warp   as Warp
 import           Servant.API
 import           Servant.Server
-import           System.Random.Shuffle     (shuffleM)
 import           Werewolf.API
 import           Werewolf.ThemeInfo
-import           Werewolf.Werewolf
+import qualified Werewolf.V1.Server.Handler as V1
 import qualified Werewolf.V2.Server.Handler as V2
 import qualified Werewolf.V3.Server.Handler as V3
+import           Werewolf.Werewolf
 
 
 showIndex :: Werewolf BL.ByteString
 showIndex = liftIO $ BL.readFile "static/index.html"
 
-appendTheme :: ThemeInfo -> Werewolf ()
-appendTheme themeInfo = unless (T.null $ theme themeInfo) $ do
-    s <- (++ [themeInfo]) <$> get
-    put s
-    saveBackup s
-
-throwNoThemeError :: MonadThrow m => m a
-throwNoThemeError = throw $ err400 {errBody = "NoThemeError"}
-
-getCurrentTheme :: Werewolf Text
-getCurrentTheme = get >>= \case
-    [] -> throwNoThemeError
-    (x:_) -> return (theme x)
-
-getAnswer :: Werewolf Text
-getAnswer = get >>= \case
-    [] -> throwNoThemeError
-    (x:_) -> return $ name x
-
-shuffleTheme :: Werewolf ()
-shuffleTheme = get >>= liftIO . shuffleM >>= put
-
-nextTheme :: Werewolf ()
-nextTheme = do
-    get >>= \case
-        [] -> throwNoThemeError
-        (x:xs) -> appendHistory x >> put xs >> saveBackup xs
-    where
-    appendHistory :: MonadIO m => ThemeInfo -> m ()
-    appendHistory = liftIO . T.appendFile "history.log" . (<> "\n") . TL.toStrict . encodeToLazyText
-
-saveBackup :: MonadIO m => [ThemeInfo] -> m ()
-saveBackup = liftIO . writeFile "backup.txt" . show
-
-showAll :: Werewolf [ThemeInfo]
-showAll = get
-
-loadTheme :: Werewolf ()
-loadTheme = loadBackup >>= put
-
-dispose :: Werewolf ()
-dispose = put []
-
 server :: ServerT API Werewolf
 server = showIndex
-    :<|> appendTheme
-    :<|> getCurrentTheme
-    :<|> getAnswer
-    :<|> shuffleTheme
-    :<|> nextTheme
-    :<|> showAll
-    :<|> loadTheme
-    :<|> dispose
+    :<|> V1.handler
     :<|> V2.handler
     :<|> V3.handler
 
 hoistWerewolf :: TVar [ThemeInfo] -> ServerT API Werewolf -> Server API
 hoistWerewolf s = hoistServer api $ fmap fst . flip runWerewolf s
 
-loadBackup :: (MonadIO m, MonadCatch m) => m [ThemeInfo]
-loadBackup = (read <$> liftIO (readFile "backup.txt")) `catchAny` const (return [])
-
 runServer :: IO ()
 runServer = do
     putStrLn "Listening on port 8080"
-    themeInfo <- loadBackup
+    themeInfo <- V1.loadBackup
     s <- atomically $ newTVar themeInfo
     Warp.run 8080 . serve api $ hoistWerewolf s server
--- src/Werewolf/API.hs
+++ src/Werewolf/API.hs
@@ -2,25 +2,17 @@
 {-# LANGUAGE TypeOperators #-}
 module Werewolf.API where
 
-import qualified Data.ByteString.Lazy as BL
-import           Data.Proxy           (Proxy (..))
-import           Data.Text            (Text)
+import qualified Data.ByteString.Lazy   as BL
+import           Data.Proxy             (Proxy (..))
 import           Servant.API
-import           Servant.HTML         (HTML)
-import           Werewolf.ThemeInfo   (ThemeInfo)
+import           Servant.HTML           (HTML)
+import qualified Werewolf.V1.Server.API as V1
 import qualified Werewolf.V2.Server.API as V2
 import qualified Werewolf.V3.Server.API as V3
 
 
 type API = Get '[HTML] BL.ByteString
-    :<|> "v1" :> "seiheki" :> ReqBody '[JSON, FormUrlEncoded] ThemeInfo :> Post '[JSON] ()
-    :<|> "v1" :> "seiheki" :> "theme" :> Get '[JSON] Text
-    :<|> "v1" :> "seiheki" :> "name" :> Get '[JSON] Text
-    :<|> "v1" :> "seiheki" :> "shuffle" :> Post '[JSON] ()
-    :<|> "v1" :> "seiheki" :> "next" :> Post '[JSON] ()
-    :<|> "v1" :> "seiheki" :> "all" :> Get '[JSON] [ThemeInfo]
-    :<|> "v1" :> "seiheki" :> "load" :> Post '[JSON] ()
-    :<|> "v1" :> "seiheki" :> "dispose" :> Post '[JSON] ()
+    :<|> V1.API
     :<|> V2.API
     :<|> V3.API
 
